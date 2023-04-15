c0d14b2a5b40c85d13ecb1898e1550a06daa9e2f
Lukáš Kratochvíl <lukas.kratochvil@zat.cz>
2023-04-15 07:57:09 +0200

    Add minor change

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,3 @@
 import { readable, writable } from 'svelte/store';
 
-export const isMobile = readable(true); // TODO: Vyřešit nějak skrze build-in feature (asi někden a $app/environment)
+export const isMobile = readable(false); // TODO: Vyřešit nějak skrze build-in feature (asi někden a $app/environment)
