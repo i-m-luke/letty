0f417bbacf56efe09fcdc72faa80185e63be2028
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2023-05-12 09:27:42 +0200

    Add hooks.server.ts implementation

---
+++ src/handlers.ts
@@ -0,0 +1,117 @@
+//#region OOP HANDLER
+
+export interface IHandler {
+  handle(route: string): boolean;
+}
+
+export abstract class BaseRouteHandler implements IHandler {
+  private nextHandler: IHandler | null = null;
+  private route: string;
+
+  constructor(route: string) {
+    this.route = route;
+  }
+
+  protected abstract process(): boolean;
+
+  handle(route: string): boolean {
+    if (route.startsWith(this.route)) {
+      return this.process();
+    }
+
+    if (this.nextHandler != null) {
+      return this.nextHandler.handle(route);
+    }
+
+    return false;
+  }
+
+  setNextHandler(handler: IHandler) {
+    return (this.nextHandler = handler);
+  }
+}
+
+export class AppHandler extends BaseRouteHandler {
+  constructor() {
+    super("/app");
+  }
+
+  override process(): boolean {
+    // PROCESS REQUEST ...
+    console.log("AppHandler performed");
+    return true;
+  }
+}
+
+export class ApiHandler extends BaseRouteHandler {
+  constructor() {
+    super("/api");
+  }
+
+  override process(): boolean {
+    // PROCESS REQUEST ...
+    console.log("ApiHandler performed");
+    return true;
+  }
+}
+
+//#endregion
+
+//#region FP HANDLER
+
+// export function handle()
+
+// enum HandleFailReason {
+// 	NotEven = '0',
+// 	SmallerThanZero = '1'
+// }
+
+// type HandleResult<TError> = {
+// 	ok: boolean;
+// 	error: TError;
+// };
+
+// /**
+//  * Handle creator initialization: initialize request type and error type
+//  * @returns
+//  */
+// const initHandlerCreator = <TReq, TError extends string>() => {
+// 	// returns handle creator
+// 	return function (fn: (request: TReq) => HandleResult<TError>) {
+// 		// returns final handle
+// 		return function (request: TReq) {
+// 			const res = fn(request);
+// 			if (res.ok) return request;
+// 			throw new Error(res.error);
+// 		};
+// 	};
+// };
+
+// const createHandlerFn = initHandlerCreator<number, HandleFailReason>();
+
+// const handlerFn01 = createHandlerFn((request: number) => {
+// 	return { ok: request > 0, error: HandleFailReason.SmallerThanZero };
+// });
+
+// const handlerFn02 = createHandlerFn((request: number) => {
+// 	return { ok: request % 2 === 0, error: HandleFailReason.NotEven };
+// });
+
+// const handleNumber = (num: number) => {
+// 	try {
+// 		handlerFn01(handlerFn02(num));
+// 	} catch (err) {
+// 		switch (err) {
+// 			case HandleFailReason.NotEven:
+// 				console.log('action after number not event');
+// 			case HandleFailReason.SmallerThanZero:
+// 				console.log('action after number not larger than zero');
+// 		}
+// 	}
+// };
+
+// handleNumber(-1); // NOK, console log
+// handleNumber(3); // NOK, console log
+// handleNumber(2); // OK, no console log
+
+//#endregion
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,61 +1,16 @@
 import { startDatabase } from "$db";
 import { log } from "$lib/logging";
+import { ApiHandler, AppHandler } from "./handlers";
 
 await startDatabase()
   .then(() => log("Database is running.."))
   .catch((err) => log("Error while starting database. Error: " + err));
 
-// export function handle()
+const appHandler = new AppHandler();
+const apiHandler = new ApiHandler();
+appHandler.setNextHandler(apiHandler);
 
-// enum HandleFailReason {
-// 	NotEven = '0',
-// 	SmallerThanZero = '1'
-// }
-
-// type HandleResult<TError> = {
-// 	ok: boolean;
-// 	error: TError;
-// };
-
-// /**
-//  * Handle creator initialization: initialize request type and error type
-//  * @returns
-//  */
-// const initHandlerCreator = <TReq, TError extends string>() => {
-// 	// returns handle creator
-// 	return function (fn: (request: TReq) => HandleResult<TError>) {
-// 		// returns final handle
-// 		return function (request: TReq) {
-// 			const res = fn(request);
-// 			if (res.ok) return request;
-// 			throw new Error(res.error);
-// 		};
-// 	};
-// };
-
-// const createHandlerFn = initHandlerCreator<number, HandleFailReason>();
-
-// const handlerFn01 = createHandlerFn((request: number) => {
-// 	return { ok: request > 0, error: HandleFailReason.SmallerThanZero };
-// });
-
-// const handlerFn02 = createHandlerFn((request: number) => {
-// 	return { ok: request % 2 === 0, error: HandleFailReason.NotEven };
-// });
-
-// const handleNumber = (num: number) => {
-// 	try {
-// 		handlerFn01(handlerFn02(num));
-// 	} catch (err) {
-// 		switch (err) {
-// 			case HandleFailReason.NotEven:
-// 				console.log('action after number not event');
-// 			case HandleFailReason.SmallerThanZero:
-// 				console.log('action after number not larger than zero');
-// 		}
-// 	}
-// };
-
-// handleNumber(-1); // NOK, console log
-// handleNumber(3); // NOK, console log
-// handleNumber(2); // OK, no console log
+export async function handle({ event, resolve }) {
+  appHandler.handle(event.url.pathname); // TODO: handle should use resolve and return result then
+  return await resolve(event);
+}
