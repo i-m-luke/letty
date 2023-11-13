43bb16539eeb5af4bcca1974474a46bbab86aaec
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2023-11-13 07:39:52 +0100

    Add some expe stuff

--- src/routes/app/expe/+page.svelte
+++ src/routes/app/expe/+page.svelte
@@ -1,25 +1,26 @@
 <!-- TOTO PŮJDE POUŽÍT PRO DYNAMICKÉ SESTAVOVÁNÍ DIALOGU PODLE PŘEDANÝCH DAT -->
 <script lang="ts">
-   import ComponentA from "./ComponentA.svelte";
-   import ComponentB from "./ComponentB.svelte";
-
-   enum ComponentType {
-      A,
-      B,
-   }
-
-   let type: ComponentType;
-
-   const getComponent = (type: ComponentType) => {
-      switch (type) {
-         case ComponentType.A:
-            return ComponentA;
-         case ComponentType.B:
-            return ComponentB;
-      }
-   };
 </script>
 
-<button on:click={() => (type = ComponentType.A)}>A</button>
-<button on:click={() => (type = ComponentType.B)}>B</button>
-<svelte:component this={getComponent(type)}>SOME DATA</svelte:component>
+<div class="w-72 h-96">
+   <div class="absolute z-10 w-72 h-96 grid grid-flow-row grid-cols-3 rounded-lg">
+      <!-- top-line -->
+      <div class="" />
+      <div class="bg-horizontal-gradient" />
+      <div class="" />
+      <!-- mid-line -->
+      <div class="row-span-2 bg-vertical-gradient" />
+      <div class="row-span-2 bg-yellow-300" />
+      <div class="row-span-2 bg-vertical-gradient" />
+      <!-- bot-line -->
+      <div class="" />
+      <div class="bg-horizontal-gradient" />
+      <div class="" />
+   </div>
+   <div class="absolute grid grid-flow-col grid-rows-2 z-0 w-72 h-96">
+      <div class="bg-corner-gradient" />
+      <div class="bg-corner-gradient" />
+      <div class="bg-corner-gradient" />
+      <div class="bg-corner-gradient" />
+   </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ export default {
     fontFamily: {
       a: ["Nunito"],
     },
+    extend: {
+      backgroundImage: {
+        "corner-gradient":
+          "radial-gradient(circle, rgba(5,46,22,1) 0%, rgba(21,128,61,1) 20%, rgba(11,64,31,1) 32%, rgba(0,0,0,1) 45%, rgba(12,71,34,1) 57%, rgba(34,197,94,1) 100%)",
+        "vertical-gradient":
+          "linear-gradient(90deg, rgba(34,197,94,1) 0%, rgba(21,128,61,1) 25%, rgba(5,46,22,1) 50%, rgba(21,128,61,1) 75%, rgba(34,197,94,1) 100%)",
+        "horizontal-gradient":
+          "linear-gradient(0deg, rgba(34,197,94,1) 0%, rgba(21,128,61,1) 25%, rgba(5,46,22,1) 50%, rgba(21,128,61,1) 75%, rgba(34,197,94,1) 100%)",
+      },
+    },
   },
   plugins: [],
 };
