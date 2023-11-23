4df71b6fc370940b2dfbccc45485433cabeaa83c
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2023-11-23 08:38:40 +0100

    Add minor changes to Response.ts

--- src/routes/app/(app)/Response.ts
+++ src/routes/app/(app)/Response.ts
@@ -1,18 +1,33 @@
 import { z } from "zod";
 
-const ResponseSchema = z.object({
+export const ResponseSchema = z.object({
   success: z.boolean(),
 });
 
-const SuccessulResponseSchema = z.object({
+export const SuccessulResponseSchema = z.object({
   data: z.any(),
 });
 
-const UnsuccessfulResponseSchema = z.object({
+export const UnsuccessfulResponseSchema = z.object({
   error: z.string(),
   issues: z.array(z.string().length(1, { message: "" })),
 });
 
-type Response =
+export type Response =
   | ({ success: true } & z.infer<typeof SuccessulResponseSchema>)
   | ({ success: false } & z.infer<typeof UnsuccessfulResponseSchema>);
+
+// NOTE:
+// Klient dostane odpověď od serveru
+// Provede validaci objektu Response
+// Poté nahledné do successfull a na základě hodnoty provede další akci (zpracuje buď data, nebo error a issue)
+const clientSide = (_res: Response) => {
+  const res = ResponseSchema.parse(_res);
+  if (res.success) {
+    const { data } = SuccessulResponseSchema.parse(res);
+    // ...
+  } else {
+    const { error, issues } = UnsuccessfulResponseSchema.parse(res);
+    // ...
+  }
+};
