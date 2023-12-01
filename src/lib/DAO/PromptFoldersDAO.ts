f184f1c8cc31080129fc6cb6755d21b9e6dbf1ed
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2023-12-01 12:22:57 +0100

    Add some changes

--- src/lib/DAO/PromptFoldersDAO.ts
+++ src/lib/DAO/PromptFoldersDAO.ts
@@ -1,8 +1,8 @@
-import { BaseDBNodeDAO } from "./BaseDBNodeDAO";
+import BaseDAO from "./BaseDAO";
 import type { Db as DB } from "mongodb";
 import { FolderSchema, type Folder, type NewFolder } from "$types";
 
-export default class PromptFoldersDAO extends BaseDBNodeDAO<Folder, NewFolder> {
+export default class PromptFoldersDAO extends BaseDAO<Folder, NewFolder> {
   constructor(db: DB) {
     super(db, "promptFolders", FolderSchema);
   }
--- src/lib/DAO/ThreadFoldersDAO.ts
+++ src/lib/DAO/ThreadFoldersDAO.ts
@@ -1,8 +1,8 @@
-import { BaseDBNodeDAO } from "./BaseDBNodeDAO";
+import BaseDAO from "./BaseDAO";
 import type { Db as DB } from "mongodb";
 import { FolderSchema, type Folder, type NewFolder } from "$types";
 
-export default class ThreadFoldersDAO extends BaseDBNodeDAO<Folder, NewFolder> {
+export default class ThreadFoldersDAO extends BaseDAO<Folder, NewFolder> {
   constructor(db: DB) {
     super(db, "threadFolders", FolderSchema);
   }
